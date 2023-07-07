89f838971fb03084bbc93ecf04f89bdbfeaf6b5c
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-07-07 17:31:34 -0300

    self._length retirado

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -5,27 +5,24 @@ class Queue(AbstractQueue):
     def __init__(self):
         """Inicialize sua estrutura aqui"""
         self._data = list()
-        self._length = 0
 
     def __len__(self):
         """Aqui irá sua implementação"""
-        return self._length
+        return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
         self._data.append(value)
-        self._length += 1
 
     def dequeue(self):
         """Aqui irá sua implementação"""
         if len(self._data) == 0:
             return None
-        self._length -= 1
         return self._data.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
-        if self._length == 0 or index >= self._length or index < 0:
+        if len(self._data) == 0 or index >= len(self._data) or index < 0:
             raise IndexError("Índice Inválido ou Inexistente")
 
         return self._data[index]
