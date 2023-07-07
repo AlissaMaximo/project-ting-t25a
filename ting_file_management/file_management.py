73e5cc23a6baa0d846d75c90698e3ff0c72a3538
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-07-07 17:17:17 -0300

    Copiando e colando file_management da PR/branch anterior para tentar corrigir erro

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file) as file:
+                return file.read().split("\n")
+        except FileNotFoundError:
+            return print(
+                f"Arquivo {path_file} não encontrado", file=sys.stderr
+            )
+    else:
+        return print("Formato inválido", file=sys.stderr)
