4f43a3188a3f0bfc4f0ac436043c16e1228adae8
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-07-09 00:07:04 -0300

    search_by_word feito

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -26,3 +26,22 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result = []
+
+    for file in instance._data:
+        word_occurence = []
+
+        for line_index, line in enumerate(file["linhas_do_arquivo"], start=1):
+            if re.search(word, line, re.IGNORECASE):
+                word_occurence.append({"linha": line_index, "conteudo": line})
+
+        if word_occurence:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": word_occurence,
+                }
+            )
+
+    return result
